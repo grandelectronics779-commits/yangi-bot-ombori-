4c027993ed8feca600348f526ba17ac1c4337958
grandelectronics779-commits <grandelectronics779@gmail.com>
2026-03-27 06:49:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from flask import Flask
 
-# ---------- TOKEN VA PORT ----------
-BOT_TOKEN  = os.getenv("8572454769:AAGLqkS2l62r29oLMRYQ6KBfUxsgAdxv1sI")
+BOT_TOKEN  = "8572454769:AAGLqkS2l62r29oLMRYQ6KBfUxsgAdxv1sI"
 PORT       = int(os.getenv("PORT", 10000))
 WEBAPP_URL = "https://grandelectronics779-commits.github.io/yangi-bot-ombori-/"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# ---------- FLASK (Render uchun) ----------
 app = Flask(__name__)
 
 @app.route("/")
@@ -21,13 +19,11 @@ def home():
 def flask_thread():
     app.run(host="0.0.0.0", port=PORT, debug=False, use_reloader=False)
 
-# ---------- TUGMA ----------
 def tugma():
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("📦 Omborxonani ochish", web_app=WebAppInfo(url=WEBAPP_URL)))
     return markup
 
-# ---------- BOT BUYRUQLARI ----------
 @bot.message_handler(commands=["start"])
 def start(msg):
     bot.send_message(msg.chat.id,
@@ -38,7 +34,6 @@ def start(msg):
 def boshqa(msg):
     bot.send_message(msg.chat.id, "👇 Tugmani bosing:", reply_markup=tugma())
 
-# ---------- ISHGA TUSHIRISH ----------
 threading.Thread(target=flask_thread, daemon=True).start()
 print("✅ Server ishga tushdi, port:", PORT)
 
